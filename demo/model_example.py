4ffd8776bea067f8f38df33855326c7781634575
abilash <abysemail@zohomail.in>
2026-07-14 18:20:16 +0530

    Add example model with file saving functionality

---
+++ demo/model_example.py
@@ -0,0 +1,10 @@
+from pymvc import model
+
+class example (model.model):
+    "this is an example for model "
+    filename = "helo.txt"
+
+    def save_to_file( self,text):
+           file = open(self.filename,"w")
+           file.write(text)
+           file.close()
