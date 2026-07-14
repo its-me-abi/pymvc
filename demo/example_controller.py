a7356c6547146c9c61e715a3a3c43a58f7753cec
abilash <abysemail@zohomail.in>
2026-07-14 18:18:53 +0530

    Import view and model in example_controller.py

---
+++ demo/example_controller.py
@@ -0,0 +1,16 @@
+from pymvc import view
+from pymvc import model
+
+class example (controller.controller) :
+    
+    "this is an example usage for contoller"
+    
+    def run(self):
+        
+        try:
+            text = self.view.ask_question("enter some text it will be saved to file")
+            self.model.save_to_file(text)
+            self.view.show_text("successfully writen to file")
+            
+        except Exception as error:
+            self.view.show_text("an error occuered while saving to file")
