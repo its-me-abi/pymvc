b426bef8183ef8eabb4a9ee01014664489f6218e
abilash <abysemail@zohomail.in>
2026-07-14 18:15:56 +0530

    Refactor controller class structure

--- controller.py
+++ controller.py
@@ -9,16 +9,4 @@ class controller  :
     def run(self):
         pass  #controller run view and model functions
 
-class example (controller) :
-    
-    "this is an example usage for contoller"
-    
-    def run(self):
-        
-        try:
-            text = self.view.ask_question("enter some text it will be saved to file")
-            self.model.save_to_file(text)
-            self.view.show_text("successfully writen to file")
-            
-        except Exception as error:
-            self.view.show_text("an error occuered while saving to file")
+
